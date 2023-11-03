8c30f2c57d8e03f6f64d61d4b99d9afcfcff2113
Dongli Liu <dongliliu0@gmail.com>
2023-11-02 23:14:20 -0700

    finished 3 core algorithms

--- Matrix.py
+++ Matrix.py
@@ -27,18 +27,18 @@ class Matrix:
                 matrix[i] = [v1 + v2 for v1, v2 in zip(matrix[i], matrix[i-1])]
         return matrix
     
-    def _print_matrix(self, matrix):
+    def _print_matrix(self):
         for i in range(3):
-            print(matrix[i])
+            print(self.matrix[i])
    
     '''
-    calculation algorithms
+    3 core calculation algorithms
     '''
     
-    def _sort_matrix(self, matrix):
+    def _sort_matrix(self):
         orders = [0,1,2]
         lookup = {}
-        matrix = sorted(matrix, key=lambda row:(row.count(0)), reverse=True)
+        matrix = sorted(self.matrix, key=lambda row:(row.count(0)), reverse=True)
         for i in range(3):
            for j in range(3):
                if matrix[i][j] != 0 and j in orders:
@@ -47,13 +47,20 @@ class Matrix:
                    break
         sorted_matrix = []
         sorted_matrix = [lookup[k] for k in range(3)]
+        self.matrix = sorted_matrix
+    
+    def _element_to_one(self, row_index, elem_index):
+        # divide a number to turn the target element into 1 
+        element = self.matrix[row_index][elem_index]
+        new_row = [e/element for e in self.matrix[row_index]]
+        self.matrix[row_index] = new_row
+    
+    def _element_to_zero(self, target_row_index, elem_index, assis_row_index):
+        # Apply -a*Rx + Ry to turn the target element into 0
+        element = self.matrix[target_row_index][elem_index]
+        combiner = [-a*element for a in self.matrix[assis_row_index]]
+        new_row = [c*t for c,t in zip(combiner, self.matrix[target_row_index])]
+        self.matrix[target_row_index] = new_row
         
-        return sorted_matrix
-    
-    def _element_to_one(self, row, index):
-        return [e/row[index] for e in row]
-    
-
-       
     def solve_matrix(self):
         pass
